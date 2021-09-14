c18e24cb7b86215accc16bb39201e7ce24fb10f0
IPI-y-endo <67990167+IPI-y-endo@users.noreply.github.com>
2021-09-14 17:59:06 +0900

    Initial commit

---
+++ src/components/organisms/user/UserCard.jsx
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+import { Card } from "../../atoms/card/Card";
+import { UserIconWithName } from "../../molecules/user/UserIconWithName";
+
+export const UserCard = (props) => {
+  const { user } = props;
+  //atomsフォルダ内Cardからstyleを受け取る
+  //moleculesフォルダのUserIcomWithNameが画像と名前の表示
+  //atomsフォルダのCardがカード形式での表示を行う
+  return (
+    <Card>
+      <UserIconWithName image={user.image} name={user.name} />
+      <SDl>
+        <dt>メール</dt>
+        <dd>{user.email}</dd>
+        <dt>TEL</dt>
+        <dd>{user.phone}</dd>
+        <dt>会社名</dt>
+        <dd>{user.company.name}</dd>
+        <dt>WEB</dt>
+        <dd>{user.website}</dd>
+      </SDl>
+    </Card>
+  );
+};
+
+const SDl = styled.dl`
+  text-align: left;
+  dt {
+    float: left;
+  }
+  dd {
+    padding-left: 32px;
+    padding-bottom: 8px;
+    overflow-wrap: break-word;
+  }
+`;
---
+++ src/components/pages/Users.jsx
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+import { SearchInput } from "../molecules/SearchInput";
+import { UserCard } from "../organisms/user/UserCard";
+
+//ユーザー一覧で表示するダミーデータ
+const users = [...Array(10).keys()].map((val) => {
+  return {
+    id: val,
+    name: `endo-${val}`,
+    image: "https://source.unsplash.com/2l0CWTpcChI",
+    email: "11111@aaa.com",
+    phone: "123-456-789",
+    company: {
+      name: "テスト株式会社"
+    },
+    website: "https://google.com"
+  };
+});
+
+export const Users = () => {
+  return (
+    <SContainer>
+      <SUserArea>
+        <h2>ユーザー一覧</h2>
+        <SearchInput />
+        {users.map((user) => (
+          <UserCard key={users.id} user={user} />
+        ))}
+      </SUserArea>
+    </SContainer>
+  );
+};
+
+const SContainer = styled.div`
+  display: flex;
+  flex-direction: center;
+  align-items: center;
+  padding: 24px;
+`;
+
+const SUserArea = styled.div`
+  padding-top: 40px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat() (aut-fit, minmax() (200px, 1fr));
+  grid-gap: 20px;
+`;
